2ebb4f8bfd0ad3044c012f8f5273c0845840ded0
Conor Hayes <ch1987@live.ie>
2015-01-30 14:09:19 +0000

    Added JeroMQ jar, made connection to queue

--- src/NotifyBidders.java
+++ src/NotifyBidders.java
@@ -1,11 +1,20 @@
 import com.mongodb.*;
 import java.net.UnknownHostException;
+import org.jeromq.ZMQ;
+import org.jeromq.ZMQ.*;
 
 public class NotifyBidders {
-    private final String SERVER_NAME = "localhost";
+    private final String SUBSCRIBER_ADDRESS = "tcp://127.0.0.1:1001", SERVER_NAME = "localhost";
     private final int PORT_NUMBER = 27018;
 
-    public static void main(String[] args){ new NotifyBidders().connect(); }
+    public static void main(String[] args){ new NotifyBidders().subscribe(); }
+
+    private void subscribe() {
+        Context context = ZMQ.context(1);
+        Socket subscriber = context.socket(ZMQ.SUB);
+        subscriber.connect(SUBSCRIBER_ADDRESS);
+        System.out.println("Connected");
+    }
 
     private void connect(){
         MongoClient client = null;
